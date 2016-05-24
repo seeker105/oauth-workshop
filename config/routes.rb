b009c747ea8dfdd79817dd5f5364d0dcd424d399
Christopher Soden <karavol27@gmail.com>
2016-05-23 19:37:36 -0600

    OAuth login/logout is working

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users,  only: [:index, :show]
 
   get "/auth/twitter", as: :twitter_login
-  
+  get "/auth/twitter/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
 end
